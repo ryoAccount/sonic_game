9074ee10ea004bf7bd66463d44a36466ca07f7de
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-10-23 01:54:55 +0900

    19:33

---
+++ src/kaplayCtx.js
@@ -0,0 +1,16 @@
+import kaplay from "kaplay";
+
+export const k = kaplay({
+  width: 1920,
+  height: 1080,
+  letterbox: true,
+  background: [0, 0, 0],
+  global: false,
+  touchToMouse: true,
+  buttons: {
+    keyboard: ["space"],
+    mouse: "left",
+  },
+});
+
+export default k;
